b16fffce6e99741b8949dcd688b08ad9d6fa78bf
jmkim <wjdals0304@gmail.com>
2022-05-25 10:54:17 +0900

    Initial Commit

---
+++ SwiftgenTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftgenTest
+//
+//  Created by 김정민 on 2022/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
